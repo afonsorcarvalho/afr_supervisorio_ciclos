9bf7c4bbf2acbb76df62be8bc6065bda4043223d
afonsorcarvalho <afonsorcarvalho@github.com>
2025-09-15 14:56:25 -0300

    update

--- fita_digital/reader_fita_digital/reader_fita_digital_afr13.py
+++ fita_digital/reader_fita_digital/reader_fita_digital_afr13.py
@@ -311,6 +311,7 @@ class ReaderFitaDigitalAfr13(ReaderFitaDigitalInterface):
                 'ESTERILIZANDO',
                 'LAVAGEM',
                 'AERACAO',
+                'HIPERVENTILACAO',
                 'CICLO FINALIZADO'
                
                
